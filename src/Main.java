a2a1cfdf3f0e6c398b808a44610ad0dabacc0674
Semenov_Atryom <arsem.personal@gmail.com>
2022-02-18 12:53:13 +0300

    Early tests has been deleted

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         TicTacToe board = new TicTacToe();
-        printField(board.getField());
-        System.out.println(board.makeMove(1, 1));
-        printField(board.getField());
-        System.out.println(board.makeMove(1, 1));
-        System.out.println(board.makeMove(1, 2));
-        printField(board.getField());
-        System.out.println(board.makeMove(2, 1));
-        System.out.println(board.makeMove(2, 2));
-        System.out.println(board.makeMove(3, 1));
-        System.out.println(board.makeMove(2, 2));
-        printField(board.getField());
-        board.newGame();
-        board.makeMove(1, 2);
-        board.makeMove(1, 1);
-
-        board.makeMove(2, 2);
-        board.makeMove(2, 1);
-
-        board.makeMove(3, 1);
-        board.makeMove(3, 2);
-
-        board.makeMove(2, 3);
-        board.makeMove(1, 3);
-
-        System.out.println(board.makeMove(3, 3));
-        printField(board.getField());
-
-        board.newGame();
+        
         board.makeMove(3, 1);
         board.makeMove(1, 2);
 
